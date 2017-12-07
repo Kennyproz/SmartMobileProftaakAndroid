42f9477ed7d6f167d9e1aee858c34ebc8784cd14
Kennyproz <k.odigwegomez@student.fontys.nl>
2017-12-07 13:02:10 +0100

    Added bluetooth check, vibrtaor/bluetooth/playing sound works and is tested

--- mobile/src/main/java/com/example/ken/smartmobileproftaakandroid/Hardware.java
+++ mobile/src/main/java/com/example/ken/smartmobileproftaakandroid/Hardware.java
@@ -1,6 +1,8 @@
 package com.example.ken.smartmobileproftaakandroid;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Build;
@@ -13,22 +15,38 @@ import android.os.Vibrator;
 
 public class Hardware {
     Vibrator vibrator;
+    BluetoothAdapter bluetoothAdapter;
     MediaPlayer mediaPlayer;
     Context context;
 
-
+    @SuppressLint("ServiceCast")
     public Hardware (Context con, MediaPlayer mp){
         this.mediaPlayer = mp;
         this.vibrator = (Vibrator)con.getSystemService(Context.VIBRATOR_SERVICE);
+        this.bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();// (BluetoothAdapter)con.getSystemService(Context.BLUETOOTH_SERVICE); //
         this.context = con;
     }
 
-    public void vibrate(int seconds){
+
+    //source https://stackoverflow.com/questions/13950338/how-to-make-an-android-device-vibrate
+    public void startVibrate(){
+        long[] pattern = {0, 100, 1000, 300, 200, 100, 500, 200, 100};
+        if(Build.VERSION.SDK_INT >= 26){
+            vibrator.vibrate(VibrationEffect.createOneShot(60000,10));
+        } else {
+            vibrator.vibrate(pattern,0);
+        }
+    }
+
+    public void startVibrate(int seconds){
         if(Build.VERSION.SDK_INT >= 26){
             vibrator.vibrate(VibrationEffect.createOneShot(seconds*100,10));
         } else {
             vibrator.vibrate(seconds*100);
         }
+    }
+    public void stopVibrate() {
+        vibrator.cancel();
     }
 
     public void startMedia(){
@@ -38,5 +56,19 @@ public class Hardware {
     public void stopMedia() {mediaPlayer.stop();}
 
     public void pauseMedia() {mediaPlayer.pause();}
+
+    public String bluetoothState(){
+        if (bluetoothAdapter == null){
+            return "Apparaat ondersteunt geen bluetooth.";
+        } else {
+            if (bluetoothAdapter.isEnabled()){
+                return "Bluetooth is verbonden.";
+            }
+            else {
+                return "Bluetooth is niet verbonden.";
+            }
+        }
+    }
+
 
 }
--- mobile/src/main/java/com/example/ken/smartmobileproftaakandroid/MainActivity.java
+++ mobile/src/main/java/com/example/ken/smartmobileproftaakandroid/MainActivity.java
@@ -5,11 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Hardware hardware;
     Switch mediaSwitch;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +19,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initialize();
-
     }
     @Override
     public void onPause() {
         super.onPause();
-        hardware.stopMedia();
+     //   hardware.stopMedia();
 
+    }
+
+    public void initialize(){
+        MediaPlayer mp = MediaPlayer.create(this,R.raw.alarm);
+        hardware = new Hardware(this,mp);
+        mediaSwitch = (Switch)findViewById(R.id.swMediaPlayer);
+        tv = (TextView)findViewById(R.id.tvBluetooth);
+        tv.setText(hardware.bluetoothState());
     }
 
     public void playMediaPlayer(View view){
@@ -37,22 +46,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public boolean alarm (Boolean bool){
+        tv.setText(hardware.bluetoothState());
         if (bool){
             hardware.startMedia();
+            hardware.startVibrate();
             Toast.makeText(this, "Alarm started", Toast.LENGTH_SHORT).show();
          return true;
         }
         else {
             hardware.pauseMedia();
+            hardware.stopVibrate();
             Toast.makeText(this, "Alarm paused", Toast.LENGTH_SHORT).show();
             return false;
         }
     }
 
-    public void initialize(){
-        MediaPlayer mp = MediaPlayer.create(this,R.raw.alarm);
-        hardware = new Hardware(this,mp);
-        mediaSwitch = (Switch)findViewById(R.id.swMediaPlayer);
-    }
+
 
 }

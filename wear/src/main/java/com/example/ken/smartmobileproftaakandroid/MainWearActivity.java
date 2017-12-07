2d94bab82017a2b173a1b79f13c84dd944d71dd2
Maarten Blömer <lucmaar@hotmail.com>
2017-12-07 15:56:22 +0100

    Vibration + Sound
    Got vibratino to work on watch, detect speaker method. No speakers available on this watch.

--- wear/src/main/java/com/example/ken/smartmobileproftaakandroid/MainWearActivity.java
+++ wear/src/main/java/com/example/ken/smartmobileproftaakandroid/MainWearActivity.java
@@ -1,7 +1,14 @@
 package com.example.ken.smartmobileproftaakandroid;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.AudioDeviceInfo;
+import android.media.AudioManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.wearable.activity.WearableActivity;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainWearActivity extends WearableActivity {
@@ -17,5 +24,49 @@ public class MainWearActivity extends WearableActivity {
 
         // Enables Always-on
         setAmbientEnabled();
+
+        Initialize();
+    }
+
+    public void Initialize(){
+
+        Switch turnOnSound = findViewById(R.id.switch1);
+        Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        long[] vibrationPattern = {0, 500, 50, 300};
+        //-1 - don't repeat
+        final int indexInPatternToRepeat = -1;
+
+        if(turnOnSound.isChecked()){
+            vibrator.vibrate(vibrationPattern, indexInPatternToRepeat);
+        }
+        else{
+            vibrator.vibrate(100);
+        }
+
+        turnOnSound.setChecked(true);
+
+        PlaySoundVibrate();
+    }
+
+    //https://developer.android.com/training/wearables/wearable-sounds.html
+    public boolean PlaySoundVibrate(){
+        PackageManager packageManager = context.getPackageManager();
+        AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+
+        // Check whether the device has a speaker.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // Check FEATURE_AUDIO_OUTPUT to guard against false positives.
+            if (!packageManager.hasSystemFeature(PackageManager.FEATURE_AUDIO_OUTPUT)) {
+                return false;
+            }
+
+            AudioDeviceInfo[] devices = audioManager.getDevices(AudioManager.GET_DEVICES_OUTPUTS);
+            for (AudioDeviceInfo device : devices) {
+                if (device.getType() == AudioDeviceInfo.TYPE_BUILTIN_SPEAKER) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }

046f412f3bde6d47768547d66c12d2f37415f508
Kennyproz <k.odigwegomez@student.fontys.nl>
2018-01-11 12:44:56 +0100

    Added settings

--- mobile/src/main/java/com/example/ken/smartmobileproftaakandroid/MainActivity.java
+++ mobile/src/main/java/com/example/ken/smartmobileproftaakandroid/MainActivity.java
@@ -155,5 +155,9 @@ public class MainActivity extends AppCompatActivity {
                 this.tvBluetooth.setText("Onbekende bluetooth status");
         }
     }
-    
+
+    public void settings(View view){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
 }

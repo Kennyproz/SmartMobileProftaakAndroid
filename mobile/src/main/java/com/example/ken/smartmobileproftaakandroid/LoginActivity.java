4f71c8fa6c6c2c6872d0b14c5219c8e8ec256b63
stefvandesande <stef.vandesande@student.fontys.nl>
2018-01-11 12:39:54 +0100

    test

--- mobile/src/main/java/com/example/ken/smartmobileproftaakandroid/LoginActivity.java
+++ mobile/src/main/java/com/example/ken/smartmobileproftaakandroid/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.example.ken.smartmobileproftaakandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by Ken on 11-1-2018.
@@ -13,5 +15,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void Login(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }

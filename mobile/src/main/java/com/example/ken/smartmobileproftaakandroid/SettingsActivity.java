19549e7715d38d62405f276035db3c36ec1bf9a2
Kennyproz <k.odigwegomez@student.fontys.nl>
2018-01-11 12:58:49 +0100

    Added button pressed actions

--- mobile/src/main/java/com/example/ken/smartmobileproftaakandroid/SettingsActivity.java
+++ mobile/src/main/java/com/example/ken/smartmobileproftaakandroid/SettingsActivity.java
@@ -2,6 +2,8 @@ package com.example.ken.smartmobileproftaakandroid;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by Ken on 11-1-2018.
@@ -12,5 +14,13 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    public void resetColors(View view) {
+        Toast.makeText(this, "Kleuren zijn teruggezet!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void returnView(View view) {
+        super.onBackPressed();
     }
 }

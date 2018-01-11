17a889d2f871b9c684dc4fbaa3e439ab404c84c9
Kennyproz <k.odigwegomez@student.fontys.nl>
2018-01-11 10:54:54 +0100

    Added Login screen

---
+++ mobile/src/main/java/com/example/ken/smartmobileproftaakandroid/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.ken.smartmobileproftaakandroid;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ken on 11-1-2018.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
